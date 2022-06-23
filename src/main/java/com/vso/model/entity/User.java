0a2788113c3fee54d83a6d34e010f7fe9a8e07ae
MartinMarkov05 <92870604+MartinMarkov05@users.noreply.github.com>
2022-06-23 10:38:31 +0300

    added xml file
    plus database created

--- src/main/java/com/vso/model/entity/User.java
+++ src/main/java/com/vso/model/entity/User.java
@@ -1,4 +1,25 @@
 package com.vso.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "facebookUsers")
 public class User {
+    @Id
+    @Column
+    private int id;
+
+    public User() {
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
